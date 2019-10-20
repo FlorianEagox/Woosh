c9bcbf63daa5ad7fe970eb6fd94bfc4fef7b6dd7
Seth Painter <seth@vps186104.vps.ovh.ca>
2019-10-19 22:04:46 -0500

    used sscweb to get list of nasa satellites

--- js/main.js
+++ js/main.js
@@ -1,7 +1,25 @@
-let displayLocation = document.querySelector("#location");
-if(navigator.geolocation)
-	setInterval(() => navigator.geolocation.getCurrentPosition(location => {
-		displayLocation.textContent = `${location.coords.latitude}, ${location.coords.longitude}`;
-	}), 250); 
-else
-	displayLocation.textContent = "You need to give this website permission to use your GPS"
+const displayLocation = document.querySelector("#location");
+const apiRoot = "https://sscweb.sci.gsfc.nasa.gov/WS/sscr/2";
+const satellites = document.querySelector("#satellites");
+
+updateLocation();
+fetch(apiRoot + "/spaseObservatories", {headers: {'Accept': 'application/json'}})
+.then(res => res.json())
+.then(satellite => {
+	satellite = satellite.Observatory[1];
+	satellite.forEach(satellite => {
+		const li = document.createElement("li");
+		li.textContent = satellite.Name;
+		satellites.appendChild(li);
+	});
+})
+
+
+function updateLocation() {
+	if (navigator.geolocation)
+		setInterval(() => navigator.geolocation.getCurrentPosition(location => {
+			displayLocation.textContent = `${location.coords.latitude}, ${location.coords.longitude}`;
+		}), 250);
+	else
+		displayLocation.textContent = "You need to give this website permission to use your GPS"
+}
